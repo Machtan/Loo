c980b96cf56ee950338b64dbf7834b3f514ddbeb
Machtan <jako3047@gmail.com>
2016-03-02 15:15:40 +0100

    Initial commit

---
+++ Assets/scripts/Customer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Customer : MonoBehaviour {
+
+    public Sprite stand;
+    public GameObject destination;
+    public float move_speed = 1.0f;
+    public float vx = 0.0f;
+    public float vy = 0.0f;
+    protected SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float delta = Time.deltaTime;
+        Vector3 direction = (destination.transform.position - transform.position).normalized;
+        vx = direction.x * move_speed;
+        vy = direction.y * move_speed;
+        transform.position += new Vector3(vx * delta, vy * delta, 0);
+
+        // Dynamic direction setting
+        if (Mathf.Abs(vx) > Mathf.Abs(vy)) {
+            if (vx > 0) {
+                transform.up = Vector3.right;
+            } else if (vx < 0){
+                transform.up = Vector3.left;
+            }
+        } else {
+            if (vy > 0) {
+                transform.up = Vector3.up;
+            } else if (vy < 0){
+                transform.up = Vector3.down;
+            }
+        }
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float move_speed = 1.0f;
+    public Sprite stand;
+
+    protected float vx = 0.0f;
+    protected float vy = 0.0f;
+    protected new SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        transform.up = Vector3.left;
+	}
+        
+	
+	// Update is called once per frame
+	void Update () {
+        // Up
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (! Input.GetKey(KeyCode.DownArrow)) {
+                vy = move_speed;
+                transform.up = Vector3.up;
+            } else {
+                vy = 0.0f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            if (! Input.GetKey(KeyCode.DownArrow)) {
+                vy = 0.0f;
+            } else {
+                vy = -move_speed;
+                transform.up = Vector3.down;
+            }
+        }
+
+        // Down
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            if (! Input.GetKey(KeyCode.UpArrow)) {
+                vy = -move_speed;
+                transform.up = Vector3.down;
+            } else {
+                vy = 0.0f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            if (! Input.GetKey(KeyCode.UpArrow)) {
+                vy = 0.0f;
+            } else {
+                vy = move_speed;
+                transform.up = Vector3.up;
+            }
+        }
+
+        // Right
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (! Input.GetKey(KeyCode.LeftArrow)) {
+                vx = move_speed;
+                transform.up = Vector3.right;
+            } else {
+                vx = 0.0f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            if (! Input.GetKey(KeyCode.LeftArrow)) {
+                vx = 0.0f;
+            } else {
+                vx = -move_speed;
+                transform.up = Vector3.left;
+            }
+        }
+
+        // Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (! Input.GetKey(KeyCode.RightArrow)) {
+                vx = -move_speed;
+                transform.up = Vector3.left;
+            } else {
+                vx = 0.0f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+            if (! Input.GetKey(KeyCode.RightArrow)) {
+                vx = 0.0f;
+            } else {
+                vx = move_speed;
+                transform.up = Vector3.right;
+            }
+        }
+
+        float delta = Time.deltaTime;
+        transform.position += new Vector3(vx * delta, vy * delta, 0);
+
+        // Dynamic direction setting
+        if (Mathf.Abs(vx) > Mathf.Abs(vy)) {
+            if (vx > 0) {
+                transform.up = Vector3.right;
+            } else if (vx < 0){
+                transform.up = Vector3.left;
+            }
+        } else {
+            if (vy > 0) {
+                transform.up = Vector3.up;
+            } else if (vy < 0){
+                transform.up = Vector3.down;
+            }
+        }
+	}
+}
